ad50f50c79bd507c6d09ba617bb986baef26a15e
ArazHMMTV <tu7R7YDYS@code.edu.az>
2024-03-28 00:08:44 +0400

    yarimciq task

---
+++ Department.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+
+
+    // burdan asagi komek almisam
+    internal class Department
+    {
+        public Employee[] Employees;
+        public Department()
+        {
+            Employees = new Employee[0];
+        }
+
+
+        public void AddEmployee(Employee employer)
+        {
+            Array.Resize(ref Employees, Employees.Length+1);
+            Employees[Employees.Length-1] = employer;
+        }
+
+
+        public void ShowEmployeeInfo(Employee employer)
+        {
+            Console.WriteLine($"Name: {employer.Name}");
+            Console.WriteLine($"Name: {employer.Surname}");
+            Console.WriteLine($"Name: {employer.Age}");
+            Console.WriteLine($"Name: {employer.DepartmentName}");
+            Console.WriteLine($"Name: {employer.Salary}");
+
+        }
+
+
+        public Employee[] GetAllEmployees()
+        {
+            return Employees;
+        }
+
+        // burani basa duse bilmedim
+        public void GetAllEmployeesBySalary(int minSalary,int maxSalary)
+        {
+
+        }
+
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class Employee
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string DepartmentName;
+        public int Salary;
+
+
+        public Employee(string name,string surname,string age,string departmentname,string salary)
+        {
+            Name = name;
+            
+        }
+    }
+}
